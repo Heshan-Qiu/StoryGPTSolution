58dff10a851276ba663331dee4ac2bce1d76f539
Heshan <heshan.chiu@gmail.com>
2023-08-29 23:37:20 +1200

    Remove unused projects

--- StoryGPTEntityAPI/Models/Tag.cs
+++
@@ -1,9 +0,0 @@
-namespace StoryGPTEntityAPI.Models
-{
-    public class Tag
-    {
-        public long Id { get; set; }
-        public long MetaDataId { get; set; }
-        public string TagName { get; set; } = null!;
-    }
-}
